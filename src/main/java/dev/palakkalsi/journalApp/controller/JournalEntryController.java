d454496170ead05e448b35646864dcf0b83867e3
palakkalsi <67413531+palakkalsi@users.noreply.github.com>
2024-06-12 01:17:44 +0530

    commit

--- src/main/java/dev/palakkalsi/journalApp/controller/JournalEntryController.java
+++ src/main/java/dev/palakkalsi/journalApp/controller/JournalEntryController.java
@@ -1,42 +1,65 @@
 package dev.palakkalsi.journalApp.controller;
 
 import dev.palakkalsi.journalApp.entity.JournalEntry;
+import dev.palakkalsi.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @GetMapping
     public List<JournalEntry> getAll(){
-        return new ArrayList<>(journalEntries.values());
+        return journalEntryService.getAll();
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry){
-        journalEntries.put(myEntry.getId(), myEntry);
-        return true;
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+        return myEntry;
     }
 
     @GetMapping("/id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable Long myId){
-        return journalEntries.get(myId);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+
+        if(journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/id/{myId}")
-    public void deleteJournalEntryById(@PathVariable Long myId){
-        journalEntries.remove(myId);
+    public boolean deleteJournalEntryById(@PathVariable ObjectId myId){
+       journalEntryService.deleteById(myId);
+       return true;
     }
 
     @PutMapping("/id/{myId}")
-    public void updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry updatedEntry){
-        journalEntries.put(myId,updatedEntry);
+    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry){
+
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+
+        if(old != null){
+            old.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent()!=null && newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+        }
+        journalEntryService.saveEntry(old);
+        return old;
     }
 }
---
+++ src/main/java/dev/palakkalsi/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package dev.palakkalsi.journalApp.repository;
+
+import dev.palakkalsi.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
